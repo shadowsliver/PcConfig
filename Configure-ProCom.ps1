5d53ada3a76ca318eb57f1180f7905957a0e76a3
Michel Michaux <michel.michaux@student.pxl.be>
2025-11-19 15:50:52 +0100

    addes a description on the CSV's

--- Configure-ProCom.ps1
+++ Configure-ProCom.ps1
@@ -16,7 +16,45 @@
   Author:         Michel Michaux
   Creation Date:  23/10/2025
   Purpose/Change: Version 1.01
-  
+
+  This script needs to be run as Administrator. Executionpolicy should be set to RemoteSigned, Bypass or Unrestricted.
+
+  If there are issues with the script, run it in terminal (openen PowerShell as Administrator).
+  Use cd or Set-Location to navigate to the script folder
+  .\Configure-ProCom.ps1
+
+  Quick mode applies a predefined set of configurations without user interaction.
+
+  If Winget is not working properly, use the debug mode to fix it.
+
+  Openeing the windows store page and updating via the debug menu (option 3) usually works.
+
+  Extra installations can be placed in the 'install' folder next to the script and configure the installations using the following files.
+  .\Install\installations.csv
+  EXAMPLE:
+  /////////////////////////////////////
+  software;file;arguments
+  SOFTWARENAME;FILENAME.EXE;ARGUEMENTS
+  SOFTWARENAME;FILENAME.EXE;ARGUEMENTS
+  SOFTWARENAME;FILENAME.EXE;ARGUEMENTS
+  /////////////////////////////////////
+  Arguements can be empty
+
+  Extra configurations can be placed in the 'config' folder next to the script.
+  .\config\netdrive.csv
+  EXAMPLE:
+  /////////////////////////////////////
+  Drive;Path
+  N;\\server\data
+  B;\\test\Folder1
+  /////////////////////////////////////
+  .\config\users.csv
+  EXAMPLE:
+  /////////////////////////////////////
+  user;fullname;password
+  JDoe;John Doe;Password123
+  /////////////////////////////////////
+
 .EXAMPLE
   Configure-ProCom.ps1
   Configure-ProCom.ps1 -Quick
@@ -86,6 +124,7 @@ $knownBugs = @(
   "Some minor info displays may not show up correctly in certain Windows versions.",
   "If a windows update fails, the system may not reboot automatically. The update failing is a microsof/windows issue, not a script issue."
 )
+
 #-----------------------------------------------------------[Functions]------------------------------------------------------------
 function Run {  
   While ( $true) {
